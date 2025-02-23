15c06ee5b238cebabac9f82948e2eeb0196c9082
shrishan9669 <shrishan9669@gmail.com>
2025-02-23 23:01:09 +0530

    Update signup.tsx

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -17,20 +17,27 @@ export default function Signup() {
 
 
   return (
-    <div className="w-full h-screen  flex justify-center bg-gradient-to-b   from-slate-400 to-slate-600 items-center">
+    <div
+    style={{backgroundImage:'url(https://images.unsplash.com/photo-1423784346385-c1d4dac9893a?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzF8fHRlY2hub2xvZ3l8ZW58MHx8MHx8fDA%3D)',
+      backgroundPosition:'center',
+      backgroundSize:'cover'
+    }}
+    className="w-full h-screen  flex justify-center   items-center">
 
-      <div className="w-[80%]  bg-gradient-to-r from-slate-300  rounded-3xl h-[94%]  to-orange-200 p-10 flex justify-around">
+      <div
+      
+      className="w-[80%]   bg-inherit   rounded-3xl h-[94%]   p-10 flex justify-center items-center">
 
         {/* signup component */}
       <div className="flex flex-col w-full sm:w-[450px] bg-inherit  rounded-lg  px-8  space-y-6">
 
-<h1 className="text-3xl font-thin text-center text-gray-800">Create an Account</h1>
+<h1 className="text-3xl font-bold text-center text-white">Create an Account</h1>
 
 
 <div className="space-y-6">
   {/* Name input  */}
   <div className="flex flex-col">
-    <label htmlFor="name" className="text-sm font-light pl-4 text-gray-600">Name</label>
+    <label htmlFor="name" className="text-sm font-bold pl-4 text-white">Name</label>
     <input
       onChange={(e) => setName(e.target.value)}
       required
@@ -42,11 +49,11 @@ export default function Signup() {
   </div>
   {/* Email Input */}
   <div className="flex flex-col">
-    <label htmlFor="email" className="text-sm font-light pl-4 text-gray-600">Email</label>
+    <label htmlFor="email" className="text-sm font-light pl-4  text-white">Email</label>
     <input
       onChange={(e) => setEmail(e.target.value)}
       required
-      type="email"
+      type="email" text-white
       id="email"
       className="mt-2 p-3 border border-gray-300 rounded-3xl focus:outline-none focus:ring-2 focus:ring-indigo-500"
       placeholder="Enter your email"
@@ -55,14 +62,14 @@ export default function Signup() {
 
   {/* Password Input */}
   <div className="flex flex-col">
-    <label htmlFor="password" className="text-sm font-light pl-4 text-gray-600">Password</label>
+    <label htmlFor="password" className="text-sm font-light pl-4 text-white ">Password</label>
   <div className="flex items-center pr-3 mt-2  border bg-white justify-between rounded-3xl w-full focus:outline-none focus:ring-2 focus:ring-indigo-500">
       <input
         onChange={(e) => setPassword(e.target.value)}
         required
         type={state}
         id="password"
-        className="w-full p-3 rounded-3xl"
+        className="w-full p-3 rounded-3xl focus:ring-2 focus:ring-indigo-500"
         placeholder="Enter your password"
       />
       {state === 'password' ? (
@@ -81,7 +88,7 @@ export default function Signup() {
 
   {/* description */}
   <div className="flex flex-col">
-    <label className="text-sm font-light pl-4 text-gray-600">Write your bio</label>
+    <label className="text-sm font-light pl-4 text-white">Write your bio</label>
     <textarea onChange={(e)=> setDesc(e.target.value)} className="mt-2 p-3 border border-gray-300 rounded-3xl w-full focus:outline-none focus:ring-2 focus:ring-indigo-500" placeholder="Write about you.." rows={4}></textarea>
   </div>
 
@@ -96,7 +103,7 @@ export default function Signup() {
         setBuf(true);
         try {
           const user = await axios({
-            url: 'https://backend-j5f0.onrender.com/user/create',
+            url: 'http://localhost:3000/user/create',
             method: "POST",
             data: {
               email: email,
@@ -124,10 +131,10 @@ export default function Signup() {
 
   {/* Login Link */}
   <div className="flex justify-center items-center space-x-2">
-    <span className="text-sm text-gray-500">Already have an account?</span>
+    <span className="text-sm text-white ">Already have an account?</span>
     <button
       onClick={() => window.location.href = ('/login')}
-      className="text-sm text-indigo-600 hover:text-indigo-500 font-semibold"
+      className="text-sm text-white font-bold hover:text-pink-600 transition-all duration-200 "
     >
       Login
     </button>
@@ -137,9 +144,9 @@ export default function Signup() {
          
 
 {/* ai generated image */}
-        <div >
-              <img src="./signupai.webp" className="h-[620px] w-[550px] rounded-3xl"   alt="" />
-        </div>
+        {/* <div >
+              <img src="https://plus.unsplash.com/premium_photo-1661898424988-a5f6d40d3db2?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fHRlY2hub2xvZ3l8ZW58MHx8MHx8fDA%3D" className="h-[620px] w-[550px] rounded-3xl"   alt="" />
+        </div> */}
       </div>
       
     </div>
